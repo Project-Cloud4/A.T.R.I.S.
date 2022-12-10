739fc7f6d4a72ff88689bc2f9296416cc8f8cda6
georgiamurariu <georgiamurariu@gmail.com>
2022-12-10 15:37:44 +0200

    NavBarAdmin

---
+++ components/navbaradmin.js
@@ -0,0 +1,19 @@
+function NavBarAdmin() {
+  return (
+    <div className="navbar bg-base-100  w-max sm:w-screen flex flex-row">
+      <div className="flex-1">
+        <label className="circle avatar pl-3 pr-3">
+          <div className="w-20 rounded-full">
+            <img src="/logo_admin.png" />
+          </div>
+        </label>
+        <div className=" text-xl sm:text-3xl font-mono text-accent">
+          ATRIS-ADMIN
+        </div>
+        </div>
+    </div>
+
+  );
+}
+
+export default NavBarAdmin;
