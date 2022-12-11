151423ad5fd932206e86a81b6d5e65468e6652fb
TudorZgimbau <tudor.zgimbau@gmail.com>
2022-12-11 10:54:51 +0200

    Added Ably connection

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
+import { configureAbly } from "@ably-labs/react-hooks";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    configureAbly({ key: process.env.NEXT_PUBLIC_ABLY_API_KEY });
+    setConnected(true);
+  }, []);
+
+  return <>{connected ? <Component {...pageProps} /> : "connecting...."}</>;
 }
 
 export default MyApp;
